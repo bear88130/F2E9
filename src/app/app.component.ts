24c8a97496b377c05a444035a766088bc45fde72
bear88130 <bear88130@yahoo.com.tw>
2018-08-02 22:52:26 +0800

    添加動畫效果

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'F2E9';
+export class AppComponent implements OnInit {
+  rankLevel: string;
+  IsGroup = false;
 
-  showSkill(skillName) {
-    alert('a');
+  ngOnInit() {
+    this.rankLevel = '初階新手';
+  }
+  showSkill(skillName: HTMLDivElement) {
+    alert(skillName.title);
+  }
+  showGroupUp() {
+    this.IsGroup = true;
+    setTimeout(() => {
+      this.IsGroup = false;
+    }, 700);
   }
 }
