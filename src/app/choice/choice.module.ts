c0fd01ade6f6232dbbdc9d2716d419ec4362e1e8
bear88130 <bear88130@yahoo.com.tw>
2018-08-22 16:02:09 +0800

    建立好 choice component

--- src/app/choice/choice.module.ts
+++ src/app/choice/choice.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChoiceComponent } from './choice/choice.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ChoiceComponent],
+  exports: [ChoiceComponent]
 })
 export class ChoiceModule { }
