88d2f32813665bbd7a5139c43f867064ee3b9578
bear88130 <bear88130@yahoo.com.tw>
2018-08-22 15:40:21 +0800

    移除 hash

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { async } from '../../../node_modules/@types/q';
 
 @Component({
   selector: 'app-home',
